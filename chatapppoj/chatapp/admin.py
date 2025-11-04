b30110eb808595c04b21e2dc5897befe07f61b07
MHMD304 <slymanmhamad86@gmail.com>
2025-11-04 18:20:42 +0200

    Edit admin.py

--- chatapppoj/chatapp/admin.py
+++ chatapppoj/chatapp/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Conservation)
+admin.site.register(Conversation)
 admin.site.register(Message)
